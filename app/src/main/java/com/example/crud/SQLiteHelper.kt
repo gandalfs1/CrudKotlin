19b82fe579783382f4dd451727209e3e54202183
FranklinWposs <franklinortiz@wposs.com>
2022-07-12 13:56:48 -0500

    Se crea metodo en la base de datos
    *metodo insert para agregar estudiantes a la base de datos
    *metodo para leer todos los estudiantes agregados

--- app/src/main/java/com/example/crud/SQLiteHelper.kt
+++ app/src/main/java/com/example/crud/SQLiteHelper.kt
@@ -1,8 +1,13 @@
 package com.example.crud
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.crud.models.Student
+
 
 class SQLiteHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
@@ -29,5 +34,60 @@ class SQLiteHelper(context: Context) :
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
         onCreate(db)
+    }
+
+    /**
+     * inserta estudiantes en la base de datos
+     *
+     * recibe por parametro un modelo de estudiante
+     * retorna un long
+     */
+    fun insertStudent(student: Student): Long{
+        val db = this.writableDatabase
+
+        val contentValues = ContentValues()
+        contentValues.put(ID, student.id)
+        contentValues.put(NAME, student.name)
+        contentValues.put(EMAIL, student.email)
+
+        val success = db.insert(TABLE_NAME, null, contentValues)
+        db.close()
+        return  success
+    }
+
+    /**
+     * devuelve todos los estudiantes
+     */
+    @SuppressLint("Range")
+    fun getAllStudent(): ArrayList<Student>{
+
+        val studentList : ArrayList<Student> = ArrayList()
+        val selectQuery = " SELECT * FROM $TABLE_NAME"
+        val db = this.readableDatabase
+
+        val cursor: Cursor?
+
+        try{
+            cursor = db.rawQuery(selectQuery,null)
+        }catch (e: Exception){
+            e.printStackTrace()
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+
+        var id:Int
+        var name:String
+        var email:String
+
+        if (cursor.moveToFirst()){
+            do{
+                id= cursor.getInt(cursor.getColumnIndex("id"))
+                name= cursor.getString(cursor.getColumnIndex("name"))
+                email= cursor.getString(cursor.getColumnIndex("email"))
+                val std = Student(id = id,name = name, email= email)
+                studentList.add(std)
+            }while (cursor.moveToNext())
+        }
+        return studentList
     }
 }
